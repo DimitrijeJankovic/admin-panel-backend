16e1ad8b5b07a9bd53b98f5e0de7ad3cf902352b
dimitrije.jankovic <dimitrije.jankovic@kelcix.com>
2018-08-17 11:44:09 +0200

    env

--- config/autoload/local_dev.php
+++ config/autoload/local_dev.php
@@ -1,14 +1,54 @@
 <?php
-/**
- * Local Configuration Override
- *
- * This configuration override file is for overriding environment-specific and
- * security-sensitive configuration information. Copy this file without the
- * .dist extension at the end and populate values as needed.
- *
- * @NOTE: This file is ignored from Git by default with the .gitignore included
- * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
- * credentials from accidentally being committed into version control.
- */
-
-return [];
+return [
+    'db' => [
+        'dsn' => 'mysql:dbname=admin_panel;host=localhost',
+        'username' => 'root',
+        'password' => 'fubar.909',
+        'host' => 'localhost',
+        'env' => 'development',
+        'url' => 'http://admin-panel.com:450',
+        'adapters' => [
+            'admin_panel' => [
+                'driver' => 'Pdo_Mysql',
+                'database' => 'admin_panel',
+                'username' => 'root',
+                'password' => 'fubar.909',
+                'dsn' => 'mysql:host=localhost;dbname=admin_panel',
+                'driver_options' => [
+                    1002 => 'SET NAMES \'UTF8\'',
+                ],
+            ],
+        ],
+    ],
+    'statuslib' => [
+        'array_mapper_path' => 'data/statuslib.php',
+    ],
+    'zf-oauth2' => [
+        'allow_implicit' => true,
+        'options' => [
+            'always_issue_new_refresh_token' => true,
+        ],
+        'access_lifetime' => 2592000,
+    ],
+    'view_manager' => [
+        'template_map' => [
+            'error/403' => 'module/Application/view/error/403.phtml',
+        ],
+    ],
+    'zf-mvc-auth' => [
+        'authentication' => [
+            'adapters' => [
+                'oauth' => [
+                    'adapter' => \ZF\MvcAuth\Authentication\OAuth2Adapter::class,
+                    'storage' => [
+                        'adapter' => \pdo::class,
+                        'dsn' => 'mysql:host=localhost;dbname=admin_panel',
+                        'route' => '/oauth',
+                        'username' => 'root',
+                        'password' => 'fubar.909',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
--- config/autoload/local_prod.php
+++ config/autoload/local_prod.php
@@ -1,14 +1,54 @@
 <?php
-/**
- * Local Configuration Override
- *
- * This configuration override file is for overriding environment-specific and
- * security-sensitive configuration information. Copy this file without the
- * .dist extension at the end and populate values as needed.
- *
- * @NOTE: This file is ignored from Git by default with the .gitignore included
- * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
- * credentials from accidentally being committed into version control.
- */
-
-return [];
+return [
+    'db' => [
+        'dsn' => 'mysql:dbname='.$_SERVER['RDS_DB_NAME'].';host=' . $_SERVER['RDS_HOSTNAME'] . ';port=' . $_SERVER['RDS_PORT'],
+        'username' => $_SERVER['RDS_USERNAME'],
+        'password' => $_SERVER['RDS_PASSWORD'],
+        'host' => $_SERVER['RDS_HOSTNAME'],
+        'env' => 'production',
+        'url' => '',
+        'adapters' => [
+            'be_db' => [
+                'driver' => 'Pdo_Mysql',
+                'database' => $_SERVER['RDS_DB_NAME'],
+                'username' => $_SERVER['RDS_USERNAME'],
+                'password' => $_SERVER['RDS_PASSWORD'],
+                'dsn' => 'mysql:host=' . $_SERVER['RDS_HOSTNAME'] . ';dbname=' . $_SERVER['RDS_DB_NAME'] . ';port=' . $_SERVER['RDS_PORT'],
+                'driver_options' => [
+                    1002 => 'SET NAMES \'UTF8\'',
+                ],
+            ],
+        ],
+    ],
+    'statuslib' => [
+        'array_mapper_path' => 'data/statuslib.php',
+    ],
+    'zf-oauth2' => [
+        'allow_implicit' => true,
+        'options' => [
+            'always_issue_new_refresh_token' => true,
+        ],
+        'access_lifetime' => 2592000,
+    ],
+    'view_manager' => [
+        'template_map' => [
+            'error/403' => 'module/Application/view/error/403.phtml',
+        ],
+    ],
+    'zf-mvc-auth' => [
+        'authentication' => [
+            'adapters' => [
+                'oauth' => [
+                    'adapter' => \ZF\MvcAuth\Authentication\OAuth2Adapter::class,
+                    'storage' => [
+                        'adapter' => \pdo::class,
+                        'dsn' => 'mysql:host='  . $_SERVER['RDS_HOSTNAME'] . ';dbname=' . $_SERVER['RDS_DB_NAME'] . ';port=' . $_SERVER['RDS_PORT'],
+                        'route' => '/oauth',
+                        'username' => $_SERVER['RDS_USERNAME'],
+                        'password' => $_SERVER['RDS_PASSWORD'],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
